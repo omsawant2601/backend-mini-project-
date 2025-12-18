4d4a730aaf32a5afc902bd3693e93b15e1da368e
omkar sawant <omkarsawant2612004@gmail.com>
2025-12-18 22:43:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app =express()
+const path = require('path')
+const userModel = require("./models/user")
+
+app.set("view engine","ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req, res)=>{
+  res.render("index")
+})
+app.get("/read",async (req, res)=>{
+    let users = await userModel.find()
+  res.render("read",{users})
+})
+app.post("/create", async (req,res)=>{
+    let {name,email,image} = req.body;
+
+    let createdUser =await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.redirect("/read")
+})
+app.get("/delete/:id",async function(req,res){
+    let deleteuser=  await userModel.findOneAndDelete({_id:req.params.id})
+    res.redirect('/read')
+})
+app.get("/edituser/:userid",async function(req,res){
+    let user=  await userModel.findOne({_id:req.params.userid})
+    res.render('edituser',{user})
+    console.log(user)
+})
+app.post("/update/:userid",async function(req,res){
+    const {name, email, image}= req.body;
+    let user=  await userModel.findOneAndUpdate({_id:req.params.userid},{name, email, image},{new:true})
+    res.redirect('/read')
+})
+app.listen(8080)
